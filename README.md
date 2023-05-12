fe8a733b3809dcd51c5c50914d71280fa3e1a84e
GicheruAnthony <giterugicheru@gmail.com>
2023-05-12 11:25:36 +0300

    simple calculator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ruby-tings
+
+This is a repository of ruby projects and basically me playing around with ruby to better understand it.
---
+++ simplecalculator.rb
@@ -0,0 +1,6 @@
+puts "Enter first number"
+num1 = gets.chomp().to_i
+puts "Enter second number"
+num2 = gets.chomp().to_i
+
+puts(num1 + num2)
